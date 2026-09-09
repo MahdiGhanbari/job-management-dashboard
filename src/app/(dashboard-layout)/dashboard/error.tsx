fc3f05a421fffd6b057ba372c4b2f3590760f836
MahdiGhanbari <mahdi.ghanbari.eng@gamil.com>
2026-09-09 22:23:18 +0330

    create a loading and error file for the dashboard

---
+++ src/app/(dashboard-layout)/dashboard/error.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+export default function error() {
+    return <div className="h-full w-full">
+        <div>error</div>
+    </div>
+}
--- src/app/(dashboard-layout)/dashboard/loading.tsx
+++ src/app/(dashboard-layout)/dashboard/loading.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 export default function Loading() {
     return <div className="inline-block relative translate-[-50%] top-[50%] left-[50%] select-none">
         <div className="size-8 rounded-full border-l-3 border-indigo-500 animate-spin ..."></div>
